59bf458b51be0f2243d733a12243aaab8c56fa46
xiuxiuyifan <1575243732@qq.com>
2025-06-14 22:21:43 +0800

    feat: 通过 json 递归渲染出 json

--- src/editor/components/EditArea.tsx
+++ src/editor/components/EditArea.tsx
@@ -1,10 +1,14 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useTodoStore } from '../stores/todo';
-import { useComponent } from '../stores/components';
+import { Component, useComponent } from '../stores/components';
+import { useComponentConfig } from 'antd/es/config-provider/context';
+import { useComponentConfigStore } from '../stores/component-config';
 
 export function EditArea() {
   const { list, addItem, updateItem, deleteItem } = useTodoStore();
   const { components, addComponent, updateComponentProps } = useComponent();
+
+  const { componentConfig } = useComponentConfigStore();
 
   useEffect(() => {
     useTodoStore.subscribe((state) => {
@@ -23,21 +27,43 @@ export function EditArea() {
     addComponent(
       {
         id: 333,
-        name: 'Video',
-        desc: '视频',
-        props: {},
+        name: 'Button',
+        desc: '按钮',
+        props: {
+          text: '无敌',
+        },
         children: [],
       },
       222,
     );
 
     updateComponentProps(222, {
-      title: '666666'
-    })
+      title: '666666',
+    });
   }, []);
+
+  function renderComponent(components: Component[]): React.ReactNode {
+    return components.map((component: Component) => {
+      const config = componentConfig?.[component.name];
+      if (!config?.component) {
+        return null;
+      }
+      return React.createElement(
+        config.component,
+        {
+          key: component.id,
+          ...config.defaultProps,
+          ...component.props,
+        },
+        // 递归调用，渲染儿子节点
+        renderComponent(component.children || []), // 有儿子的话继续渲染儿子节点
+      );
+    });
+  }
   return (
     <div className="h">
       <pre>{JSON.stringify(components, null, 2)}</pre>
+      {renderComponent(components)}
     </div>
   );
 }
